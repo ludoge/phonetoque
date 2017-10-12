8544caf11fa0d4d7c3b27e864cde5ff9884043ca
Ludovico <ludoalta@gmail.com>
2017-10-12 16:57:31 +0200

    Now handles words without extra pronunciations

--- Wiktionary_scraper.py
+++ Wiktionary_scraper.py
@@ -9,17 +9,20 @@ from bs4 import BeautifulSoup
 def pronunciations_from_wiktionary(word):
     soup = BeautifulSoup(requests.get('https://en.wiktionary.org/wiki/%s' % word).content, 'lxml')
 
-    first_section_break = soup.find("hr")
+    try:
+        first_section_break = soup.find("hr")
 
-    # Section switches to other languages after <hr>
-    pronunciations = first_section_break.find_all_previous('span', class_="IPA")
+        # Section switches to other languages after <hr>
+        pronunciations = first_section_break.find_all_previous('span', class_="IPA")
+
+    except AttributeError:
+        pronunciations = soup.find_all('span', class_="IPA")
 
     # Difference between the two IPA syntaxes left for later
     regex = re.compile('^[/\[].*[/\]]$')
 
     # Removing tags and eventual parasites (see: penis)
     pronunciations = [x.text for x in pronunciations if regex.search(x.text)]
-
     return pronunciations
 
 
