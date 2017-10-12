0ca07a746b6af3368810488afac37963324c82d8
Ludovico <ludoalta@gmail.com>
2017-10-12 16:25:49 +0200

    Renamed script file

---
+++ Wiktionary_scraper.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+from lxml import html
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
+def pronunciations_from_wiktionary(word):
+    soup = BeautifulSoup(requests.get('https://en.wiktionary.org/wiki/%s' % word).content, 'lxml')
+
+    first_section_break = soup.find("hr")
+
+    # Section switches to other languages after <hr>
+    pronunciations = first_section_break.find_all_previous('span', class_="IPA")
+
+    # Difference between the two IPA syntaxes left for later
+    regex = re.compile('^[/\[].*[/\]]$')
+
+    # Removing tags and eventual parasites (see: penis)
+    pronunciations = [x.text for x in pronunciations if regex.search(x.text)]
+
+    return pronunciations
+
+
+if __name__ == '__main__':
+    while True:
+        word = input("Enter an English word: \n")
+        print(pronunciations_from_wiktionary(word))
