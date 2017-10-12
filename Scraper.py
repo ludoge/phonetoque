c8a546910d1fab1fccd60b5715268dd5681127fb
Ludovico <ludoalta@gmail.com>
2017-10-12 16:00:09 +0200

    Better version using BeautifulSoup

--- Scraper.py
+++ Scraper.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[41]:
+# In[45]:
 
 
 from lxml import html
@@ -9,22 +9,40 @@ import requests
 import re
 
 
-# In[42]:
+# In[98]:
+
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(requests.get('https://en.wiktionary.org/wiki/elephant').content, 'lxml')
+
+first_section_break = soup.find("hr")
+
+pronunciations = first_section_break.find_all_previous('span', class_="IPA")
+
+pronunciations = [x.text for x in pronunciations]
+
+print(pronunciations)
+
+
+# In[102]:
 
 
 def pronunciations_from_wiktionary(word):
-    page = requests.get('https://en.wiktionary.org/wiki/%s' %word)
-    tree = html.fromstring(page.content)
-    pronunciation = []
-    for i in range(1,10):
-        pronunciation = pronunciation + tree.xpath("//*[@id=\"mw-content-text\"]/div/ul[1]/li[%s]/span/text()" %i)
-    regexp = re.compile(r'^/.*/$')
-    pronunciation = [x for x in pronunciation if regexp.search(x)]
-    return(pronunciation)
+    soup = BeautifulSoup(requests.get('https://en.wiktionary.org/wiki/%s' %word).content, 'lxml')
+
+    first_section_break = soup.find("hr")
+
+    pronunciations = first_section_break.find_all_previous('span', class_="IPA")
+    
+    regex = re.compile('^[/\[].*[/\]]$')
+
+    pronunciations = [x.text for x in pronunciations if regex.search(x.text)]
+    
+    return(pronunciations)
 
 
-# In[43]:
+# In[104]:
 
 
-print(pronunciations_from_wiktionary('python'))
+print(pronunciations_from_wiktionary('elephant'))
 
