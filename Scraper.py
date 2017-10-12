dbeb3fb5f95d158a03c862356e81fec8044ef95e
Ludovico <ludoalta@gmail.com>
2017-10-12 16:20:36 +0200

    Cleaned up into working little script, now testing

--- Scraper.py
+++ Scraper.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # In[45]:
@@ -7,42 +6,24 @@
 from lxml import html
 import requests
 import re
-
-
-# In[98]:
-
-
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(requests.get('https://en.wiktionary.org/wiki/elephant').content, 'lxml')
-
-first_section_break = soup.find("hr")
-
-pronunciations = first_section_break.find_all_previous('span', class_="IPA")
-
-pronunciations = [x.text for x in pronunciations]
-
-print(pronunciations)
-
-
-# In[102]:
 
 
 def pronunciations_from_wiktionary(word):
-    soup = BeautifulSoup(requests.get('https://en.wiktionary.org/wiki/%s' %word).content, 'lxml')
+    soup = BeautifulSoup(requests.get('https://en.wiktionary.org/wiki/%s' % word).content, 'lxml')
 
     first_section_break = soup.find("hr")
 
     pronunciations = first_section_break.find_all_previous('span', class_="IPA")
-    
+
     regex = re.compile('^[/\[].*[/\]]$')
 
     pronunciations = [x.text for x in pronunciations if regex.search(x.text)]
-    
-    return(pronunciations)
+
+    return pronunciations
 
 
-# In[104]:
-
-
-print(pronunciations_from_wiktionary('elephant'))
-
+if __name__ == '__main__':
+    while True:
+        word = input("Enter an English word: \n")
+        print(pronunciations_from_wiktionary(word))
