56566b392c012a9e01b8064e4884e8b70d7fb6fc
Ludovico <ludoalta@gmail.com>
2018-01-17 12:44:50 +0100

    refactored script posting data to DB

---
+++ scripts/post.py
@@ -0,0 +1,36 @@
+import argparse
+import src.provisioning as pr
+import src.syllables as sy
+import src.string_cleanup as sc
+import yaml
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--language', help='specify the language of the data used', required=True)
+    parser.add_argument('-i', help='path to input word list', required=True, dest='input_file')
+    parser.add_argument('--conf', help='path to configuration file for scripts', default='scripts/script_config.yml')
+    args = parser.parse_args()
+
+    with open(args.conf, encoding='utf-8') as f:
+        config = yaml.safe_load(f)
+    available_languages = config['ipa_hyphenation_dict']
+
+    if args.language not in available_languages:
+        parser.error(f"Language \"{args.language}\" not available."
+                     f"\nAvailable languages: {' ,'.join(available_languages)} ")
+
+    config['simplifying'] = config['simplifying'][args.language]
+    config['hyphenation_dict'] = config['hyphenation_dict'][args.language]
+    config['ipa_hyphenation_dict'] = config['ipa_hyphenation_dict'][args.language]
+    config['language'] = args.language
+
+    cleaner = sc.StringCleanup(config)
+    raw_data = cleaner.read_pronunciation_file_as_dict(args.input_file)
+    clean_data = {k: [cleaner.strip_separators(cleaner.cleanup(x)) for x in v] for k,v in raw_data.items()}
+
+    request = pr.PhonetoqueRequest(config)
+    request.pronunciations = clean_data
+    request.prepare_data()
+    #print(request.pronunciations)
+
+    request.post_all()
---
+++ src/provisioning.py
@@ -0,0 +1,38 @@
+import json
+import pyphen
+import requests
+
+
+class PhonetoqueRequest:
+    def __init__(self, config):
+        self.language = config['language']
+        self.hyphenation_dict = pyphen.Pyphen(lang=config['hyphenation_dict'])
+        self.ipa_hyphenation_dict = pyphen.Pyphen(lang=config['ipa_hyphenation_dict'])
+        self.url = config['server_url']
+        self.pronunciations = {}
+
+    def prepare_data(self):
+        self.pronunciations = {self.hyphenation_dict.inserted(k): [self.ipa_hyphenation_dict.inserted(x) for x in v]
+                               for k, v in self.pronunciations.items()}
+
+    def post_to_db(self, word):
+        spelling = word.replace("-","")
+        syllables = word.split("-")
+        for pronunciation in self.pronunciations[word]:
+            spelling_ipa = pronunciation.replace("-","")
+            syllables_ipa = pronunciation.split("-")
+
+            payload = {
+                "language": self.language,
+                "spelling": spelling,
+                "spelling_ipa": spelling_ipa,
+                "syllables": syllables,
+                "syllables_ipa": syllables_ipa
+            }
+            response = requests.post(f"{self.url}/{self.language}_words/", headers={'Content-Type': 'application/json'},
+                                 data=json.dumps(payload))
+            print(response.text)
+
+    def post_all(self):
+        for word in self.pronunciations:
+            self.post_to_db(word)
