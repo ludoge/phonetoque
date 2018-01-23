dac99383d332ccb9c12b609f0c6019e03fdd891d
Ludovico <ludoalta@gmail.com>
2018-01-23 13:48:32 +0100

    minor fixes to all scripts

--- scripts/post.py
+++ scripts/post.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 
     cleaner = sc.StringCleanup(config)
     raw_data = cleaner.read_pronunciation_file_as_dict(args.input_file)
-    clean_data = {k: [cleaner.strip_separators(cleaner.cleanup(x)) for x in v] for k,v in raw_data.items()}
+    clean_data = {k: [cleaner.simplify_separators(cleaner.cleanup(x)) for x in v] for k,v in raw_data.items()}
 
     request = pr.PhonetoqueRequest(config)
     request.pronunciations = clean_data
