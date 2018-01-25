830e3d8e59087b8e9fa6ff3a6e210b5ed5060955
valentinArzel <valentin.arzel@student.ecp.fr>
2018-01-25 16:08:25 +0100

    test de translittération

--- Flask/mongo_connect.py
+++ Flask/mongo_connect.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, make_response, render_template, Response
+from flask import Flask, request, jsonify, make_response, render_template, Response, redirect
 from flask_pymongo import PyMongo
 from Words import *
 import json
@@ -17,8 +17,13 @@ mongo = PyMongo(app)
 # ------------------ Routes Debug ------------------------ #
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/database', methods=['GET', 'POST'])
+def database():
     if request.method == 'POST':
         try:
             result = get_word(request.form['language'],request.form['word'])
@@ -26,8 +31,8 @@ def index():
             word = loads(JSON)
             return render_template('word.html', word=word, mod=False)
         except jinja2.exceptions.UndefinedError:
-            render_template('error.html')
-    return render_template('index.html')
+            render_template('error.html',message="Ce mot n'existe pas dans la base")
+    return render_template('database.html')
 
 
 @app.route('/modify_word', methods=['GET', 'POST'])
@@ -44,7 +49,7 @@ def modify():
         collection.insert(insert)
         return render_template('word.html',word=insert,mod=True)
     else:
-        return render_template('error.html'), 404
+        return render_template('error.html',message="Impossible d'accéder à cette page"), 404
 
 
 @app.route('/add_word', methods=['GET', 'POST'])
@@ -63,11 +68,59 @@ def new_word():
         default = {'language':'english', 'spelling':'', 'spelling_ipa':'', 'syllables':[], 'syllables_ipa':[] }
         return render_template('word.html',word=default,mod=False)
 
+@app.route('/all_words/',methods=['POST'])
+@app.route('/all_words/<language>/<spelling>/',methods=['GET'])
+def all_words(language=None,spelling=None):
+    if request.method == "POST":
+        url = "/all_words/"+request.form['language']+"/"+request.form['spelling']
+        print(url)
+        return redirect(url)
+    collection = get_collection(language).find()
+    words = []
+    for word in collection:
+        if word['spelling'] == spelling:
+            words += [word]
+    return render_template('all_words.html',all_words=words)
+
+
+@app.route('/translitteration/',methods=['POST','GET'])
+def translitterate():
+    if request.method == 'GET':
+        return render_template('translitteration.html',post=False)
+    else:
+            # on récupère les données de la requête
+        spelling = request.form['spelling']
+        language1 = request.form['language1']
+        language2 = request.form['language2']
+        if language1 == language2:
+            return render_template('error.html',message='Entrez deux langages différents pour la translittération !')
+            # on se place dans les bonnes collections
+        coll_word_1 = get_collection(language1)
+        coll_syll_1 = get_collection_syllables(language1)
+        coll_syll_2 = get_collection_syllables(language2)
+            # on récupère la prononciation du mot
+        word = coll_word_1.find_one({'spelling':spelling})
+        if word is None:
+            return render_template('error.html', message="Ce mot n'est pas répertorié !")
+        syllables_ipa1 = word['syllables_ipa']
+
+        syllables = []
+        syllables_ipa2 = []
+        for syll in syllables_ipa1:
+            # on cherche la correspondance de chaque syllabe dans la 2eme langue
+            syll1 = coll_syll_1.find_one({'spelling_ipa':syll})[language2] #phonetique dans la langue 2
+            syllables_ipa2 += [syll1]
+            syll2 = coll_syll_2.find_one({'spelling_ipa':syll1, 'language':language2})['spelling'] #orthographique dans la langue 2
+            syllables += [syll2]
+
+        return render_template('translitteration.html',post=True,word=spelling,result=syllables,language1=language1,language2=language2,syllables=word['syllables'],syllables_ipa=syllables_ipa1,syllables_ipa2=syllables_ipa2)
+
 
 # ------------------ Fonctions générales ------------------------ #
 
 
 def get_collection(language):
+    """""
     # on se place dans la collection correspondant à la langue choisie
     if language == 'english':
         collection = mongo.db.english_words
@@ -78,6 +131,24 @@ def get_collection(language):
     else:
         collection = None
     return collection
+    """
+    return mongo.db.test_valentin
+
+
+def get_collection_syllables(language):
+    """"
+     # on se place dans la collection de syllabes correspondant à la langue choisie
+    if language == 'english':
+        collection = mongo.db.english_syllables
+    elif language == 'french':
+        collection = mongo.db.french_syllables
+    elif language == 'italian':
+        collection = mongo.db.italian_syllables
+    else:
+        collection = None
+    return collection
+    """
+    return mongo.db.test_valentin
 
 
 def get_word(language,word):
@@ -98,9 +169,13 @@ def get_word(language,word):
     return output
 
 
+def delete(language,id):
+    return None
+
+
 # ------------------ Routes API ------------------------ #
 
-
+"""""
 @app.route('/<language>/', defaults={'word': ''}, methods = ['GET']) #pour avoir tous les mots dans une langue
 @app.route('/<language>/<path:word>', methods=['GET']) #pour avoir les details d'un mot
 def get_all_words(language, word):
@@ -110,7 +185,7 @@ def get_all_words(language, word):
 
 @app.route('/<language>_words/', methods=['POST'])
 def add_word(language):
-    all_words = get_collection()
+    all_words = get_collection(language)
     data = request.get_json()
     # nettoyer et transformer les donnees client
     if language == data['language']:
@@ -155,9 +230,7 @@ def get_all_syllables(language, ipa_syllable):
 
 @app.route('/<language>_syllables/', methods=['POST'])
 def add_syllables(language):
-    """
-    to add new sylables
-    """
+    #to add new sylables
     if language == 'english':
         all_syllables = mongo.db.english_syllables
     elif language == 'french':
@@ -177,6 +250,8 @@ def add_syllables(language):
     db_insert = {'ipa_syllable': ipa_syllable, 'orthographical_syllable': orthographical_syllable, 'preceding_ipa_syllable':preceding_ipa_syllable, 'following_ipa_syllable': following_ipa_syllable}
     all_syllables.insert(db_insert)
     return "The syllable {} has been added to the {} syllable database".format(ipa_syllable, language)
+"""
+
 
 if __name__ == '__main__':
     app.run(debug=True)
